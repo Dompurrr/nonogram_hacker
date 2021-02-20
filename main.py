ba54540c4106e9e4359a941a9742f55b620bc68a
Dompurrr <55359826+Dompurrr@users.noreply.github.com>
2021-02-21 00:13:33 +0500

    Added input part
    added input part in main.py, also made input.txt

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+#Начало блока ввода из файла
 f = open('input.txt', 'r')
-#На сегодня хватит, некит, дальши пиши сам
+a = f.readline()
+#Количество столбцов
+ylines = int((a.split())[1])
+#количество строк
+xlines = int((a.split())[0])
+#Листы со значениями столбиков и столбцов, где индекс (начиная с 0) соответствует строке/столбцу
+x_list = []
+y_list = []
+for i in range(xlines):
+    x_list.append([])
+for i in range(ylines):
+    y_list.append([])
+for i in range(ylines):
+    y_list[i] = (f.readline()).split()
+for i in range(xlines):
+    x_list[i] = (f.readline()).split()
+#Конец блока ввода из файла
+#Начало блока вычислений
